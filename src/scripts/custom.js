4e65b331450958a675651fbf9b29f4726b1aee13
Tracy Lee <tleecoding@gmail.com>
2016-11-01 17:07:15 -0500

    font changed

--- src/scripts/custom.js
+++ src/scripts/custom.js
@@ -33,3 +33,44 @@ $('.trusted-company-img-container')
       $('.show-title').height(iShowTitleMaxHeight);
     }); 
   }
+
+
+  document.forms[0].target = "https://vts.inxpo.com/scripts/Server.nxp?LASCmd=AI:4;F:APIUTILS!51004&PageID=79C65956-5715-46A8-9689-202CB16D279D";
+
+
+<!-- Google Code for Viewed Virtual Summit Page Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 1011114068;
+var google_conversion_language = "en";
+var google_conversion_format = "3";
+var google_conversion_color = "ffffff";
+var google_conversion_label = "3ZufCLn__moQ1MCR4gM";
+var google_remarketing_only = false;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/1011114068/?label=3ZufCLn__moQ1MCR4gM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
+
+<!-- Google Code for Registered for Virtual Summit Event Conversion Page -->
+<script type="text/javascript">
+	var google_conversion_id = 1011114068;
+	var google_conversion_language = "en";
+	var google_conversion_format = "3";
+	var google_conversion_color = "ffffff";
+	var google_conversion_label = "7Eg-CNfTkmsQ1MCR4gM";
+	var google_remarketing_only = false;
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/1011114068/?label=7Eg-CNfTkmsQ1MCR4gM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
